10b9c662e962e21f328883d327d4ae075bd98673
KarKinTal <100282563+KarKinTal@users.noreply.github.com>
2022-03-02 12:58:38 +0100

    lab2

--- main.py
+++ main.py
@@ -1,87 +1,108 @@
-#zadanie 1
-zm_d1, zm_d2 = 2, 4
-zm_f1, zm_f2 = 1.5, 0.125
-zm_s1, zm_s2 = 'herbata', 'kawa'
-zm_c1, zm_c2 = 2+1j, 3+2j
-print('Intiger: %(z1)d, %(z2)d' %{'z1':zm_d1, 'z2':zm_d2})
-print('Float: %(z1)f, %(z2)f' %{'z1':zm_f1, 'z2':zm_f2})
-print('String: %(z1)s, %(z2)s' %{'z1':zm_s1, 'z2':zm_s2})
-print('Complex: ')
-print(zm_c1)
-print(zm_c2)
+# a = 8
+# b = 7
+# if a > b:
+#     print('Liczba %(zm1)d jest większa od liczby %(zm2)d'%{'zm1':a, 'zm2':b})
+# elif a < b:
+#     print('Liczba %(zm1)d jest mniejsza od liczby %(zm2)d' % {'zm1': a, 'zm2': b})
+# else:
+#     print('Liczby %(zm1)d i %(zm2)d są równe' % {'zm1': a, 'zm2': b})
 
-#zadanie 2
-zm1 = input ("Podaj 1. liczbę: ")
-zm2 = input ("Podaj 2. liczbę: ")
-print('''Możliwe operacje:
-1 - dodawanie
-2 - odejmowanie
-3 - mnożenie
-4 - dzielenie
-5 - potęgowanie
-6 - dzielenie całkowite
-7 - reszta z dzielenia''')
-op = input ("Wybierz operacje (1-7): ")
+# if a == b:
+#     print('Liczby %(zm1)d i %(zm2)d są równe' % {'zm1': a, 'zm2': b})
+# else:
+#     print('Liczby %(zm1)d i %(zm2)d są różne' % {'zm1': a, 'zm2': b})
 
-if op == '1':
-    wynik = int(zm1) + int(zm2)
-    print(wynik)
-elif op == '2':
-    wynik = int(zm1) - int(zm2)
-    print(wynik)
-elif op == '3':
-    wynik = int(zm1) * int(zm2)
-    print(wynik)
-elif op == '4':
-    wynik = int(zm1) / int(zm2)
-    print(wynik)
-elif op == '5':
-    wynik = int(zm1) ** int(zm2)
-    print(wynik)
-elif op == '6':
-    wynik = int(zm1) // int(zm2)
-    print(wynik)
-elif op == '7':
-    wynik = int(zm1) % int(zm2)
-    print(wynik)
-else:
-    print("Nieprawidlowy operator.")
+# a = input('Podaj liczbę: ')
+# print(a)
+# print(type(a))
+# a = int (a) #rzutowanie
+# print(a)
+# print(type(a))
+# b = int(input ('Podaj liczbę: '))
+# print(b)
+# print(type(b))
+# #if (a > b) & (c > d):
 
-#zadanie 3
+# a = input('Podaj 1. liczbę: ') #kopiowanie linijki w dół: ctrl+d
+# b = input('Podaj 2. liczbę: ')
+# c = input('Podaj 3. liczbę: ')
+# d = input('Podaj 4. liczbę: ')
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# d = int(d)
+# if (a > b) & (c > d):
+#     print ('%(zm1)d jest większe od %(zm2)d i %(zm3)d jest większe od %(zm4)d' %{'zm1':a, 'zm2':b, 'zm3':c, 'zm4':d})
+# else:
+#     print ('%(zm1)d jest mniejsze od %(zm2)d lub %(zm3)d jest mniejsze od %(zm4)d' % {'zm1': a, 'zm2': b, 'zm3': c, 'zm4': d})
 
-#zadanie 4
-import math
-e=2.71
-print(e ** 10)
-x = 3.55
-y = 4.00
-print(abs(x))
-print(abs(y))
-print(math.pow(math.log(5 + math.pow(math.sin(8), 2)), 1/6))
+# for a in range(0, 7, 1):
+#     print (a)
 
-#zadanie 5
-imie = "KAROLINA"
-nazwisko = "TALAREK"
-print (imie.capitalize() + " " + nazwisko.capitalize())
+# lista = [] #deklaracja pustej listy
+# lista = ['cos', 3, 4, 3.14]
+# for a in lista:
+#     print (a)
+# else:
+#     print ("Wyświetlono wszystkie elementy listy")
 
-#zadanie 6
-tekst = "la la la"
-print (tekst.count("la"))
+# licznik = 0
+# while licznik != 11:
+#     print (licznik)
+#     licznik += 1
 
-#zadanie 7
-napis_dowolny = "Wieczko od słoika"
-print(napis_dowolny)
-print("Druga litera: " + napis_dowolny[1])
-print("Ostatnia litera: " + napis_dowolny[-1:])
+# lista = [4, 8, 5, 3, 2, 9 , 7]
+# licznik = 0
+# liczba = input('Wpisz liczbę całkowitą: ')
+# while liczba != len(lista): #funckja len zwraca liczbę elementów listy
+#     if int(liczba) - lista[licznik] == 0:
+#         print('Liczba ' + str(liczba) + ' - ' + str(lista[licznik]) + ' = 0')
+#         break
+#     else: licznik += 1
+# else:
+#     print('Żadna z wartości nie dała odpowiedniego wyniku') #jeżeli zadziałał break to ten else jest pomijany
 
-#zadanie 8
-print(tekst.split())
+# lista1 = [4, 6, 8, 1, 6, 7, 2, 9]
+# lista2 = [4, 7, 3, 5]
+# suma = []
+# for a in lista1:
+#     for b in lista2:
+#         wynik = a+b
+#         suma.append(wynik) #funkcja append dodaje wynik na koniec listy
+#     print (suma)
 
-#zadanie 9
-zs = "pomidor"
-zf = 3.14
-z16 = hex(26)
-print("%(zm1)s, %(zm2)f, %(zm3)s" %{"zm1":zs, "zm2":zf, "zm3":z16})
+# a = input("Podaj 1. liczbę: ")
+# b = input("Podaj 2. liczbę: ")
+# try:
+#     a = int(a)
+#     b = int(b)
+#     wynik = a/b
+#     print (wynik)
+# except ZeroDivisionError:
+#     print('Nie można dzielić przez zero.')
+# except ValueError:
+#     print('Nie wczytano liczby całkowitej.')
 
-liczba_szesnastkowa = 0x1f
-print('{0:x}'.format(liczba_szesnastkowa))
+lista = [4, 3.14, [1, 2, 3], 'a', 'nie', 2]
+slownik = {1:'ble', 2:30, 3:1.5, 'a':'b'}
+
+print(lista)
+print(slownik)
+
+lista.append(slownik['a'])
+print(lista)
+slownik[4]=10
+print(slownik)
+
+lista.insert(slownik[2], slownik[4])
+print(lista)
+
+lista.pop(0) #usuwa pozycje o danym numerze z listy
+slownik.pop(1)
+print(lista)
+print(slownik)
+
+lista.remove('a') #usuwa podaną rzecz z listy
+print(lista)
+
+
