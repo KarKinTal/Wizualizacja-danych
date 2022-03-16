fcd59ddfd89fb32733b71b54e7893b41924a4976
KarKinTal <100282563+KarKinTal@users.noreply.github.com>
2022-03-16 12:37:21 +0100

    lab3

--- main.py
+++ main.py
@@ -1,108 +1,109 @@
-# a = 8
-# b = 7
-# if a > b:
-#     print('Liczba %(zm1)d jest większa od liczby %(zm2)d'%{'zm1':a, 'zm2':b})
-# elif a < b:
-#     print('Liczba %(zm1)d jest mniejsza od liczby %(zm2)d' % {'zm1': a, 'zm2': b})
-# else:
-#     print('Liczby %(zm1)d i %(zm2)d są równe' % {'zm1': a, 'zm2': b})
+import math
 
-# if a == b:
-#     print('Liczby %(zm1)d i %(zm2)d są równe' % {'zm1': a, 'zm2': b})
-# else:
-#     print('Liczby %(zm1)d i %(zm2)d są różne' % {'zm1': a, 'zm2': b})
+a = []
+for x in range(0, 10):
+    a.append(x**2)
+print(a)
+a2 = [x**2 for x in range(0, 10)]
+print(a2)
 
-# a = input('Podaj liczbę: ')
-# print(a)
-# print(type(a))
-# a = int (a) #rzutowanie
-# print(a)
-# print(type(a))
-# b = int(input ('Podaj liczbę: '))
-# print(b)
-# print(type(b))
-# #if (a > b) & (c > d):
+b = []
+for x in range(0, 6):
+    b.append(3**x)
+print(b)
+b2 = [3**x for x in range(0, 6)]
+print(b2)
 
-# a = input('Podaj 1. liczbę: ') #kopiowanie linijki w dół: ctrl+d
-# b = input('Podaj 2. liczbę: ')
-# c = input('Podaj 3. liczbę: ')
-# d = input('Podaj 4. liczbę: ')
-# a = int(a)
-# b = int(b)
-# c = int(c)
-# d = int(d)
-# if (a > b) & (c > d):
-#     print ('%(zm1)d jest większe od %(zm2)d i %(zm3)d jest większe od %(zm4)d' %{'zm1':a, 'zm2':b, 'zm3':c, 'zm4':d})
-# else:
-#     print ('%(zm1)d jest mniejsze od %(zm2)d lub %(zm3)d jest mniejsze od %(zm4)d' % {'zm1': a, 'zm2': b, 'zm3': c, 'zm4': d})
+c = []
+for x in a:
+    if x % 2 == 1:
+        c.append(x)
+print(c)
+c2 = [x for x in a if x % 2 == 1]
+print(c2)
 
-# for a in range(0, 7, 1):
-#     print (a)
+# ZAGNIEŻDŻENIA
 
-# lista = [] #deklaracja pustej listy
-# lista = ['cos', 3, 4, 3.14]
-# for a in lista:
-#     print (a)
-# else:
-#     print ("Wyświetlono wszystkie elementy listy")
-
-# licznik = 0
-# while licznik != 11:
-#     print (licznik)
-#     licznik += 1
-
-# lista = [4, 8, 5, 3, 2, 9 , 7]
-# licznik = 0
-# liczba = input('Wpisz liczbę całkowitą: ')
-# while liczba != len(lista): #funckja len zwraca liczbę elementów listy
-#     if int(liczba) - lista[licznik] == 0:
-#         print('Liczba ' + str(liczba) + ' - ' + str(lista[licznik]) + ' = 0')
-#         break
-#     else: licznik += 1
-# else:
-#     print('Żadna z wartości nie dała odpowiedniego wyniku') #jeżeli zadziałał break to ten else jest pomijany
-
-# lista1 = [4, 6, 8, 1, 6, 7, 2, 9]
-# lista2 = [4, 7, 3, 5]
-# suma = []
-# for a in lista1:
-#     for b in lista2:
-#         wynik = a+b
-#         suma.append(wynik) #funkcja append dodaje wynik na koniec listy
-#     print (suma)
-
-# a = input("Podaj 1. liczbę: ")
-# b = input("Podaj 2. liczbę: ")
-# try:
-#     a = int(a)
-#     b = int(b)
-#     wynik = a/b
-#     print (wynik)
-# except ZeroDivisionError:
-#     print('Nie można dzielić przez zero.')
-# except ValueError:
-#     print('Nie wczytano liczby całkowitej.')
-
-lista = [4, 3.14, [1, 2, 3], 'a', 'nie', 2]
-slownik = {1:'ble', 2:30, 3:1.5, 'a':'b'}
-
+lista = []
+for a in [1, 2, 3]:
+    for b in [4, 5, 6]:
+        lista.append((a, b))
 print(lista)
+lista2 = [(a, b) for a in [1, 2, 3] for b in [4, 5, 6]]
+print(lista2)
+
+slownik = {'PZU': 'Państwowy zaklad ubezpieczeń',
+           'ZUS': 'Zakład ubezpieczeń społecznych',
+           'PKO': 'Państwowa kasa oszczędności'}
+slownik_odwrocony = {}
 print(slownik)
+for key, value in slownik.items():
+    slownik_odwrocony[value] = key
+print(slownik_odwrocony)
+slownik2 = {value: key for key, value in slownik.items()}
+print(slownik2)
 
-lista.append(slownik['a'])
-print(lista)
-slownik[4]=10
-print(slownik)
+# DEFINICJE FUNKCJI
 
-lista.insert(slownik[2], slownik[4])
-print(lista)
+def row_kwadratowe (a, b, c):
+    delta = b**2 - 4 * a * c
+    if delta < 0:
+        print('Brak rozwiązań')
+        return -1
+    elif delta == 0:
+        print('Jedno rozwiązanie:')
+        x = (-b)/(2 * a)
+        return x
+    else:
+        print('Dwa rozwiązania:')
+        x1 = ((-b) - math.sqrt(delta))/(2 * a)
+        x2 = ((-b) + math.sqrt(delta))/(2 * a)
+        return x1, x2
 
-lista.pop(0) #usuwa pozycje o danym numerze z listy
-slownik.pop(1)
-print(lista)
-print(slownik)
+print(row_kwadratowe(6, 1, 3))
+print(row_kwadratowe(1, 2, 1))
+print(row_kwadratowe(1, 4, 1))
 
-lista.remove('a') #usuwa podaną rzecz z listy
-print(lista)
+def czy_parzysta (x):
+    if x % 2 == 0:
+        return 'Liczba jest parzysta'
+    else:
+        return 'Liczba jest nieparzysta'
 
+print(czy_parzysta(7))
+print(czy_parzysta(4))
+
+def dl_odcinka(x1=1, y1=2, x2=3, y2=4):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+# argumenty domyślne
+print(dl_odcinka())
+# argumenty pozycyjne
+print(dl_odcinka(4, 5, 6, 9))
+# dwa argumenty pozycyjne, dwa określone
+print(dl_odcinka(2, 2, y2=7, x2=5))
+# argumenty po za kolejnością
+print(dl_odcinka(x2=6, y2=4, x1=2, y1=3))
+# dwa argumnety domyślne, dwa z nowa wartością
+print(dl_odcinka(x2=4, y2=7))
+
+def ciag(* liczby): # * - dowolna ilośc wartościowań
+    if len(liczby) == 0:
+        return 0
+    else:
+        suma = 0
+        for i in liczby:
+            suma += i
+        return suma
+
+print(ciag())
+print(ciag(1, 2, 3, 4, 5, 6, 7, 8))
+
+def co_lubie(** rzeczy): ## ** - dowolna ilośc argumentów z kluczem
+    for cos in rzeczy:
+        print('to jest')
+        print(cos)
+        print('co lubie')
+        print(rzeczy[cos])
+# cos - klucz, rzeczy[cos] - wartość na pozycji
+co_lubie(gry=['planszowe', 'kąkuterowe'], slodycze='czekolada')
 
